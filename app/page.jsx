32fd0c63c1fc2b8447aad94d865a9bd19a66268c
Salila Herath <salilaherath@gmail.com>
2023-07-23 19:03:57 +0530

    Component files and Nav compenent done

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,5 @@
+import Feed from "@components/Feed";
+
 const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
@@ -11,6 +13,8 @@ const Home = () => {
         PromptCraft is an open-source AI prompting tool for modern world to
         discover, create and share creative prompts
       </p>
+
+      <Feed />
     </section>
   );
 };
